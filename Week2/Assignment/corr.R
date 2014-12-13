2e1425ca090767802bf255a8e6c701ff8319be93
Liran Dreval <lirand411@gmail.com>
2014-12-13 18:14:39 +0200

    full cycle

---
+++ Week2/Assignment/corr.R
@@ -0,0 +1,12 @@
+corr <- function(directory, threshold = 0) {
+        cr <- c()
+        cc <- complete(directory)
+        cc_thresh <- cc[cc$nobs > threshold, ]
+        for (i in cc_thresh$id) {
+                file_name <- paste(directory, "/", sprintf("%03d", i), ".csv", sep="")
+                data <- read.csv(file_name)
+                cr <- c(cr, (cor(data$sulfate, data$nitrate,  use="pairwise.complete.obs" )))
+        
+        }
+        return(cr)
+}
