e5a3a1481b9b2cce763bac789e1a1b026648dbea
Liran Dreval <lirand411@gmail.com>
2014-12-13 16:09:23 +0200

    Week2/Assignment/pollutantmean.R

--- Week2/Assignment/pollutantmean.R
+++ Week2/Assignment/pollutantmean.R
@@ -1,16 +1,4 @@
 pollutantmean <- function(directory, pollutant, id = 1:332) {
-  ## 'directory' is a character vector of length 1 indicating
-  ## the location of the CSV files
-  
-  ## 'pollutant' is a character vector of length 1 indicating
-  ## the name of the pollutant for which we will calculate the
-  ## mean; either "sulfate" or "nitrate".
-  
-  ## 'id' is an integer vector indicating the monitor ID numbers
-  ## to be used
-  
-  ## Return the mean of the pollutant across all monitors list
-  ## in the 'id' vector (ignoring NA values)
   sum <- c()
   for(i in id) {
           ## add zeros for file normalization
@@ -29,8 +17,6 @@ pollutantmean <- function(directory, pollutant, id = 1:332) {
           # remove NA and read proper column
           na_list <- is.na(file[,pollutant])
           sum <- c(sum, file[!na_list,pollutant])
-          
   }
   mean(round(sum,3))
-  
 }
